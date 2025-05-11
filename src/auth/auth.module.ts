32ff3d073ac9252fb60996c0dcff0e71edfb570b
Abdelrhman <abdo612200320@gmail.com>
2025-05-11 21:49:14 +0300

    update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SCERET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}

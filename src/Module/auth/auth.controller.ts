ccc53e5d99b5a43889c7e0782994ef9261493a20
Abdelrhman <abdo612200320@gmail.com>
2025-05-12 04:15:14 +0300

    update auth.controller.ts

--- src/Module/auth/auth.controller.ts
+++ src/Module/auth/auth.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInhDto, SignUphDto } from './dto/create-auth.dto';
 
-@Controller('v1/auth')
+@Controller('v1/auth') // Base route for all authentication-related endpoints
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {} // Inject AuthService to handle authentication logic
 
+  /**
+   * Handles user registration (sign-up).
+   *
+   * @param signUphDto - Object containing the user's registration details.
+   * @returns A success response with the newly registered user's details.
+   */
   @Post('sign-up')
   signUp(
     @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
@@ -14,6 +20,12 @@ export class AuthController {
     return this.authService.SignUp(signUphDto);
   }
 
+  /**
+   * Handles user login (sign-in).
+   *
+   * @param signInhDto - Object containing the user's login credentials.
+   * @returns A success response with the authentication token and user details.
+   */
   @Post('sign-in')
   sigin(
     @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))

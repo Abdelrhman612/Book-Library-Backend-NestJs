9da3a07657db295aa649fc9e71c6276dbe189d41
Abdelrhman <abdo612200320@gmail.com>
2025-05-12 03:59:32 +0300

    feat: create Auth.interFace.ts

---
+++ src/auth/InterFace/Auth.interFace.ts
@@ -0,0 +1,10 @@
+export interface SignUpInterFace {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+export interface SignInInterFace {
+  email: string;
+  password: string;
+}

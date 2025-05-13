d8f9b6ec8203811dc7d1a5d698e254c26086b4b3
Abdelrhman <abdo612200320@gmail.com>
2025-05-13 16:26:00 +0300

    update Auth.interFace.ts

--- src/Module/auth/InterFace/Auth.interFace.ts
+++ src/Module/auth/InterFace/Auth.interFace.ts
@@ -2,9 +2,20 @@ export interface SignUpInterFace {
   name: string;
   email: string;
   password: string;
-  role: string;
 }
 export interface SignInInterFace {
+  email: string;
+  password: string;
+}
+export interface ResetPassword {
+  email: string;
+}
+export interface verifyCode {
+  email: string;
+  verificationCode: string;
+  code: string;
+}
+export interface changePassword {
   email: string;
   password: string;
 }

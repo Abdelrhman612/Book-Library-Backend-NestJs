73d140130aeea48bcd815ec85f4be1916bf15e3a
Abdelrhman <abdo612200320@gmail.com>
2025-05-13 19:46:42 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5001',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
-  const port = process.env.PORT || 3000; 
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 void bootstrap();

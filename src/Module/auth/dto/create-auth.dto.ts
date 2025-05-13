636c28c572676ff14a944b08627dfa5a74d0d27a
Abdelrhman <abdo612200320@gmail.com>
2025-05-13 16:25:54 +0300

    update create-auth.dto.ts

--- src/Module/auth/dto/create-auth.dto.ts
+++ src/Module/auth/dto/create-auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class SignUphDto {
   @IsString({ message: 'Name must be a string' })
@@ -12,9 +12,6 @@ export class SignUphDto {
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
-  @IsString({ message: 'Role must be a string' })
-  @IsNotEmpty({ message: ' Role is required' })
-  role: string;
 }
 export class SignInhDto {
   @IsEmail({}, { message: 'Email must be a valid email address' })
@@ -24,4 +21,29 @@ export class SignInhDto {
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
+}
+
+export class ResetPasswordto {
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+}
+export class VerifyCodeDto {
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+  @IsString({ message: 'Verification code must be a string' })
+  @IsOptional()
+  verificationCode: string;
+  @IsString({ message: 'Verification code must be a string' })
+  code: string;
+}
+export class ChangePasswordDto {
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsOptional()
+  email: string;
 }

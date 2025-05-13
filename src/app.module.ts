9fa1f937282b70af57d29dddc4c5b04e12fd27a2
Abdelrhman <abdo612200320@gmail.com>
2025-05-13 16:25:16 +0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './DataBase/prisma.module';
 import { UserModule } from './Module/user/user.module';
 import { AuthModule } from './Module/auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })

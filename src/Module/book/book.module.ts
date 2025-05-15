b9f70ff3c6e2cb1810debb8a65e274c9b5bd484d
Abdelrhman <abdo612200320@gmail.com>
2025-05-15 15:58:12 +0300

    update book.module.ts

--- src/Module/book/book.module.ts
+++ src/Module/book/book.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SCERET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES },
+    }),
+  ],
   controllers: [BookController],
   providers: [BookService],
 })

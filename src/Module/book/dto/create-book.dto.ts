626a2e9e6a9fb1106982af90b7e2e67e4089d2f2
Abdelrhman <abdo612200320@gmail.com>
2025-05-15 15:24:12 +0300

    update create-book.dto.ts

--- src/Module/book/dto/create-book.dto.ts
+++ src/Module/book/dto/create-book.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBookDto {}
+import { IsString, IsOptional } from 'class-validator';
+
+export class CreateBookDto {
+  @IsString({ message: 'Title must be a string' })
+  title: string;
+
+  @IsString({ message: 'Author must be a string' })
+  author: string;
+
+  @IsString({ message: 'Category must be a string' })
+  category: string;
+
+  @IsString({ message: 'Description must be a string' })
+  description: string;
+
+  @IsString({ message: 'image must be a valid string' })
+  image: string;
+
+  @IsOptional()
+  @IsString({ message: 'User ID must be a string if provided' })
+  userId?: string;
+}

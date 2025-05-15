ed92a5cf023bf17c6aebc483a95ac58ce9b04124
Abdelrhman <abdo612200320@gmail.com>
2025-05-15 15:23:55 +0300

    update book.controller.ts

--- src/Module/book/book.controller.ts
+++ src/Module/book/book.controller.ts
@@ -1,34 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { AuthGuard } from 'src/Guards/Auth.Guard';
+import { Roles } from 'src/decorator/role.decorator';
 
-@Controller('book')
+@Controller('v1/book')
+@UseGuards(AuthGuard)
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.bookService.create(createBookDto);
+  @Roles(['admin'])
+  create(
+    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    createBookDto: CreateBookDto,
+  ) {
+    return this.bookService.createBook(createBookDto);
   }
 
   @Get()
   findAll() {
-    return this.bookService.findAll();
+    return this.bookService.getBooks();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.bookService.findOne(+id);
+    return this.bookService.getBook(id);
   }
 
   @Patch(':id')
+  @Roles(['admin'])
   update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.bookService.update(+id, updateBookDto);
+    return this.bookService.updateBook(id, updateBookDto);
   }
 
   @Delete(':id')
+  @Roles(['admin'])
   remove(@Param('id') id: string) {
-    return this.bookService.remove(+id);
+    return this.bookService.DeleteBook(id);
   }
 }

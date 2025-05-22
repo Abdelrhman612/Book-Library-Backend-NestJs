f2884079091a8d16f5e3a3985a2441d84eea5448
Abdelrhman <abdo612200320@gmail.com>
2025-05-22 23:52:45 +0300

    update cloudinary.service.ts

--- src/Module/cloudinary/cloudinary.service.ts
+++ src/Module/cloudinary/cloudinary.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Injectable, Logger } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryResponse } from './cloudinary-response';
